d236b3f3068fb8700b9291e930c53cfa7ffc39b3
Valentine Wallace <vwallace@protonmail.com>
2021-02-03 17:51:56 -0500

    Expose 'spending onchain funds' guide

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
   someSidebar: {
       'Lightning Development Kit': ['overview', 'getting_started', 'use_cases', 'references'],
-      Guides: ['build_node', 'open_channel', 'key_mgmt', 'blockdata'],
+      Guides: ['build_node', 'open_channel', 'key_mgmt', 'blockdata', 'onchain_funds'],
   },
 };
