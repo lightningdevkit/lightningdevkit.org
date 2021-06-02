e3888b2613bcbe00558d2e6fec390e830c4be352
Jeffrey Czyz <jkczyz@gmail.com>
2021-06-02 00:36:31 -0400

    Using LDK guide

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
   someSidebar: {
       'Lightning Development Kit': ['overview', 'use_cases'],
-      Guides: ['build_node', 'build_node_rust', 'key_mgmt', 'blockdata'],
+      Guides: ['build_node', 'build_node_rust', 'using_ldk', 'key_mgmt', 'blockdata'],
   },
 };
