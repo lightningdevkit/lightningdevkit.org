d733fc2a8301177a0d624010d0d2920d50e92403
Dennis Reimann <mail@dennisreimann.de>
2021-07-27 09:16:13 +0200

    Add footer
    Closes  #61

--- docs/.vuepress/theme/components/Footer.vue
+++ docs/.vuepress/theme/components/Footer.vue
@@ -3,12 +3,47 @@
     <div class="wrap">
       <div class="wrap-border">
         <div class="inner">
-          Copyright © 2021 LDK Developers
+          <div class="footer-content">
+            <div
+              v-for="block in links"
+              :key="block.title"
+              class="footer-block"
+            >
+              <h4>{{ block.title }}</h4>
+              <div
+                v-for="item in block.children"
+                :key="item.link"
+              >
+                <NavLink :item="item" />
+              </div>
+            </div>
+          </div>
+          <p v-if="copyright" class="copyright">{{ copyright }}</p>
         </div>
       </div>
     </div>
   </footer>
 </template>
+
+<script>
+import NavLink from '@theme/components/NavLink.vue'
+
+export default {
+  name: 'Footer',
+
+  components: { NavLink },
+
+  computed: {
+    links () {
+      return this.$site.themeConfig.footer.links
+    },
+
+    copyright () {
+      return this.$site.themeConfig.footer.copyright
+    }
+  }
+}
+</script>
 
 <style lang="stylus">
 .footer
@@ -17,12 +52,31 @@
     z-index 10
   .wrap-border
     border-top var(--docs-border-dashed)
-    padding var(--docs-space-l)
-    @media (min-width: $MQMobile)
-      padding-left 19.5rem !important
-    @media (min-width: $MQNarrow)
-      padding-left 23.6rem !important
+    padding 0
+    .theme-container:not(.no-sidebar) &
+      @media (min-width: $MQMobile)
+        padding-left 17.5rem !important
+      @media (min-width: $MQNarrow)
+        padding-left 21.1rem !important
+    .theme-container.no-sidebar &
       .inner
-        padding-left 1.6rem
-        padding-right 1.6rem
+        @media (min-width: $MQNarrow)
+          max-width 48rem !important
+          margin 0 auto
+          border-left var(--docs-border-dashed)
+          border-right var(--docs-border-dashed)
+          padding var(--docs-space-l)
+  .footer-content
+    display flex
+    flex-wrap wrap
+    padding-top var(--docs-space-m)
+    @media (min-width: $MQNarrow)
+      justify-content space-between
+    .footer-block
+      padding var(--docs-space-m) var(--docs-space-xxl) var(--docs-space-m) 0
+    h4
+      margin 0 0 var(--docs-space-s)
+  .copyright
+    margin-bottom 0
+    padding-bottom var(--docs-space-l)
 </style>
