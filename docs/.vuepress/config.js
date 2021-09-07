0f97321191049ff72372f13a1631501c3edd2b6f
Dennis Reimann <mail@dennisreimann.de>
2021-09-07 10:31:07 +0200

    Extract theme

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,25 +1,12 @@
 const { resolve } = require('path')
-const footnotes = require('markdown-it-footnote')
-const implicitFigures = require('markdown-it-implicit-figures')
-const slugify = require('./slugify')
-const preprocessMarkdown = resolve(__dirname, 'preprocessMarkdown')
+const themeConfig = require('squarecrypto-vuepress-devkit-theme/config')
 
 const title = 'Lightning Dev Kit Documentation'
 const baseUrl = 'https://lightningdevkit.org'
 const githubUrl = 'https://github.com/lightningdevkit'
 const slackUrl = 'https://join.slack.com/t/lightningdevkit/shared_invite/zt-tte36cb7-r5f41MDn3ObFtDu~N9dCrQ'
 const themeColor = '#ffffff'
-const pageSuffix = '/'
-const info = { name: title }
-const extractDescription = text => {
-  if (!text) return
-  const paragraph = text.match(/^[A-Za-z].*(?:\n[A-Za-z].*)*/m)
-  return paragraph ? paragraph.toString().replace(/[\*\_\(\)\[\]]/g, '') : null
-}
-const sitemap = {
-  hostname: baseUrl,
-  exclude: ['/404.html']
-}
+
 const docsSidebar = [
   {
     title: 'Documentation',
@@ -64,6 +51,7 @@ const tutorialSidebar = [
     ],
   }
 ]
+
 const blogSidebar = [
   {
     title: 'Blog',
@@ -79,110 +67,12 @@ const blogSidebar = [
 module.exports = {
   title,
   description: 'LDK is a flexible lightning implementation with supporting batteries (or modules).',
-  head: [
-    ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1.0' }],
-    ['link', { rel: 'preload', href: '/fonts/manrope-400.woff2', as: 'font', crossorigin: true }],
-    ['link', { rel: 'preload', href: '/fonts/manrope-600.woff2', as: 'font', crossorigin: true }],
-    ['link', { rel: 'preload', href: '/fonts/ibm-plex-mono-400.woff2', as: 'font', crossorigin: true }],
-    ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/img/favicon/apple-touch-icon.png' }],
-    ['link', { rel: 'manifest', href: '/site.webmanifest' }],
-    ['link', { name: 'msapplication-config', content: '/browserconfig.xml' }],
-    ['link', { name: 'msapplication-TileColor', content: themeColor }],
-    ['link', { name: 'theme-colorr', content: themeColor }]
-  ],
-  chainWebpack (config) {
-    config.module
-      .rule('md')
-      .test(/\.md$/)
-      .use(preprocessMarkdown)
-        .loader(preprocessMarkdown)
-        .end()
-  },
-  plugins: [
-    ['seo', {
-      siteTitle: (_, $site) => $site.title,
-      title: $page => $page.title,
-      description: $page => $page.frontmatter.description || extractDescription($page._strippedContent),
-      author: (_, $site) => info,
-      tags: $page => ($page.frontmatter.tags || ['Bitcoin', 'Lightning Network', 'LDK']),
-      twitterCard: _ => 'summary',
-      type: $page => 'article',
-      url: (_, $site, path) => `${baseUrl}${path.replace('.html', pageSuffix)}`,
-      image: ($page, $site) => `${baseUrl}/card.png`
-    }],
-    ['clean-urls', {
-      normalSuffix: pageSuffix,
-      indexSuffix: pageSuffix,
-      notFoundPath: '/404.html',
-    }],
-    ['code-copy', {
-      color: '#8F979E',
-      backgroundTransition: false,
-      staticIcon: true
-    }],
-    ['sitemap', sitemap],
-    ['tabs', {
-      tabsAttributes: {
-        options: { useUrlFragment: false }
-      }
-    }],
-    ['@vuepress/medium-zoom'],
-    ['@vuepress/blog', {
-      sitemap,
-      directories: [
-        {
-          id: 'blog',
-          dirname: '_blog',
-          path: '/blog/',
-          itemPermalink: '/blog/:slug',
-          pagination: {
-            lengthPerPage: 10,
-            getPaginationPageTitle(pageNumber) {
-              return `Page ${pageNumber}`
-            }
-          }
-        },
-      ],
-      frontmatters: [
-        {
-          id: 'tags',
-          keys: ['tags'],
-          path: '/blog/tags/',
-          title: '',
-          frontmatter: {
-            title: 'Tags'
-          },
-          pagination: {
-            getPaginationPageTitle(pageNumber, key) {
-              return `${capitalize(key)} - Page ${pageNumber}`
-            }
-          }
-        },
-        {
-          id: 'author',
-          keys: ['author', 'authors'],
-          path: '/blog/author/',
-          title: '',
-          frontmatter: {
-            title: 'Authors'
-          },
-          pagination: {
-            getPaginationPageTitle(pageNumber, key) {
-              return `${key} - Page ${pageNumber}`
-            }
-          }
-        },
-      ],
-    }]
-  ],
-  markdown: {
-    extendMarkdown(md) {
-      md.use(footnotes)
-      md.use(implicitFigures)
-    },
-    pageSuffix,
-    slugify
-  },
+  theme: resolve(__dirname, '../../node_modules/squarecrypto-vuepress-devkit-theme'),
+  ...themeConfig({
+    baseUrl,
+    title,
+    themeColor
+  }),
   themeConfig: {
     domain: baseUrl,
     logo: '/img/logo.svg',
