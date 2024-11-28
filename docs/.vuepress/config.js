a1d1195167c0bd23476ea020da77dcfe459adda1
Conor Okus <conor.okus@proton.me>
2024-11-27 22:17:24 -0500

    Fix CoC broken link

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -277,7 +277,6 @@ module.exports = {
             {
               text: "Code of Conduct",
               link: "/code_of_conduct",
-              rel: "noopener noreferrer",
             },
           ],
         },
